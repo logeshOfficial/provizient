7b7eca62bfce20de3d1f3bb17d0970d078832c59
logeshOffical <iamlogeshwaran.info@gmail.com>
2026-07-05 19:12:54 +0530

    upodated

--- src/app/(marketing)/blog/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-/**
- * Individual blog post slugs redirect to the dev.to profile.
- * All articles live at https://dev.to/sreeni5018
- */
-export default function BlogPostPage() {
-  redirect("https://dev.to/sreeni5018");
-}
